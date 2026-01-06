24742f4fab40738c0b079f4bfbe74d73d6837c8f
Ben Lanning <blanning@all-mode.com>
2026-01-05 19:56:08 -0500

    clean up lint issues

--- .labrc.js
+++ .labrc.js
@@ -1,10 +1,12 @@
+'use strict';
+
 module.exports = {
-  assert: '@hapi/code',
-  coverage: true,
-  colors: true,
-  lint: true,
-  output: ['stdout', './coverage/test_output.html', './coverage/test_output.lcov'],
-  reporter: ['console', 'html', 'lcov'],
-  threshold: 100,
-  verbose: true
+    assert: '@hapi/code',
+    coverage: true,
+    colors: true,
+    lint: true,
+    output: ['stdout', './coverage/test_output.html', './coverage/test_output.lcov'],
+    reporter: ['console', 'html', 'lcov'],
+    threshold: 100,
+    verbose: true
 };
